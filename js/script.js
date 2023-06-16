48a4f03d7c2d99da693fcfc751e267c044aff52d
iankit-tanwar <tankitofficial01.gmail.com>
2023-06-16 13:54:15 +0530

    commit

--- js/script.js
+++ js/script.js
@@ -1,34 +1,54 @@
 
 
 
- //1function defination 
-        // Proper Naming Convention
-        //i.e function name are always follow camelCase 
-        function myFunctionA(x, y) { // x and y formal arguments / parameters
-            // function body
-            // every function return something
+//1function defination 
+// Proper Naming Convention
+//i.e function name are always follow camelCase 
+function myFunctionA(x, y) { // x and y formal arguments / parameters
+    // function body
+    // every function return something
 
-            return x + y;
+    return x + y;
 
-        }
+}
 
-        //x variable store the return value
+//x variable store the return value
 
-        let x = myFunctionA("Ankit"+' '+ myFunctionA(10,50)); // functions as function argument 
+let x = myFunctionA("Ankit" + ' ' + myFunctionA(10, 50)); // functions as function argument 
 
-        console.log( 'x--->',x);
+console.log('x--->', x);
 
 
-     
-        //1 function defination
 
-        function myfunctionB( c, b){
+//1 function defination
 
-            // function body 
-            // every function return something
+function myfunctionB(c, b) {
 
-            return c+b
-        }
+    // function body 
+    // every function return something
 
-        let z = myfunctionB(10,x);
-        console.log('z--->',z);
+    return c + b
+}
+
+let z = myfunctionB(10, x);
+console.log('z--->', z);
+
+
+
+function a(x){
+    return x;
+}
+
+let r1 = a(10);
+console.log(r1);
+
+function b(y,z){
+    return y +z;
+}
+ let r2 = b(50 , a(60));
+ console.log(r2);
+
+
+
+
+
