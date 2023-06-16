61988e3618a311c1d0124242e34ca74f9f85ecd6
iankit-tanwar <tankitofficial01.gmail.com>
2023-06-16 13:18:25 +0530

    commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+
+
+ //1function defination 
+        // Proper Naming Convention
+        //i.e function name are always follow camelCase 
+        function myFunctionA(x, y) { // x and y formal arguments / parameters
+            // function body
+            // every function return something
+
+            return x + y;
+
+        }
+
+        //x variable store the return value
+
+        let x = myFunctionA("Ankit"+' '+ myFunctionA(10,50)); // functions as function argument 
+
+        console.log( 'x--->',x);
+
+
+     
+        //1 function defination
+
+        function myfunctionB( c, b){
+
+            // function body 
+            // every function return something
+
+            return c+b
+        }
+
+        let z = myfunctionB(10,x);
+        console.log('z--->',z);
